57a5b09024ff0faad9e19fb7bc19c28422405f31
Ansh Patel <1210anshpatel@gmail.com>
2021-01-01 15:56:48 +0530

    Added server.py. this file will act as a cron job for raspberry pi. and run at startup
    ADDED AN ENDLESS LOOP TO DETECT ANY POSSIBLE CHANGES EVERY 1.86 SECONDS

--- API/server.py
+++ API/server.py
@@ -8,3 +8,17 @@ db = mysql.connector.connect(
     password="ansh1Rutu"
 )
 cursor  = db.cursor()
+
+while True:
+    cursor.execute("SELECT RESPONSE FROM u257284371_iot.MESSAGES WHERE ID = 1;")
+    response = cursor.fetchone()
+    db.commit()
+    if response[0].startswith('CHANGED'):
+        detect_change(response[0])
+        print('buffering')
+        sleep(5)
+        system('clear')
+
+    else:
+        # print(response[0])
+        sleep(1.5)
