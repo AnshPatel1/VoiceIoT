d5cd6ae69de0eed5eb65d9f4c6eeceb240282795
Ansh Patel <1210anshpatel@gmail.com>
2021-01-01 15:44:01 +0530

    Added an endless loop and tested for auto change detection in mysql server

--- API/client.py
+++ API/client.py
@@ -7,3 +7,24 @@ db = mysql.connector.connect(
     user="u257284371_iot",
     password="ansh1Rutu"
 )
+
+while True:
+    pin = int(input('SELECT A GPIO CHANNEL: '))
+    if pin not in [1, 14, 15] and pin < 28:
+        MODE = int(input('ENTER MODE ANALOG(A) / DIGITAL(D):  '))
+        state = int(input('ENTER STATE:  '))
+        if MODE == 'A':
+            print('digital_set_gpio({}, {})'.format(pin, state))
+            digital_set_gpio(pin, state)
+            print('DONE!')
+            sleep(3)
+        if MODE == 'D':
+            print('analog_set_gpio({}, {})'.format(pin, state))
+            analog_set_gpio(pin, state)
+            print('DONE!')
+            sleep(3)
+    else:
+        print("INVALID GPIO. IT DOESN'T EXIST\n"
+              'TRY AGAIN')
+        sleep(3)
+        system('clear')
