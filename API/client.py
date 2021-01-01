991d0f501179b0fa30821dfdc51db70dba289732
Ansh Patel <1210anshpatel@gmail.com>
2021-01-01 15:45:38 +0530

    tested and added a function digital_set_gpio() to send "VIRTUAL" requests to mysql database

--- API/client.py
+++ API/client.py
@@ -8,6 +8,20 @@ db = mysql.connector.connect(
     password="ansh1Rutu"
 )
 
+
+def digital_set_gpio(gpio_id, state):
+    if not db.is_connected():
+        print('DATABASE TIMED OUT. RECONNECTING......')
+        db.reconnect(attempts=2, delay=0)
+    cursor = db.cursor()
+    cursor.execute("UPDATE u257284371_iot.MESSAGES SET RESPONSE = 'CHANGED: {}' WHERE ID = 1;".format(str(gpio_id)))
+    cursor.execute("UPDATE u257284371_iot.GPIO_STREAM SET DIGITAL_STATUS = {} WHERE GPIO_ID = {};".format(str(state),
+                                                                                                          str(gpio_id)))
+    cursor.execute("UPDATE u257284371_iot.GPIO_STREAM SET PWM_STATUS = NULL WHERE GPIO_ID = {};".format(str(gpio_id)))
+    cursor.execute("UPDATE u257284371_iot.MESSAGES SET RESPONSE = 'D' WHERE ID = 2;")
+    cursor.close()
+    db.commit()
+
 while True:
     pin = int(input('SELECT A GPIO CHANNEL: '))
     if pin not in [1, 14, 15] and pin < 28:
