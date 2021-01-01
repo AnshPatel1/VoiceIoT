786d911adc72332608524421dc819c38337e901f
Ansh Patel <1210anshpatel@gmail.com>
2021-01-01 15:42:37 +0530

    made a initial connection to remote mysql server with client.py

---
+++ API/client.py
@@ -0,0 +1,9 @@
+from os import system
+from time import sleep
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="185.201.11.44",
+    user="u257284371_iot",
+    password="ansh1Rutu"
+)
